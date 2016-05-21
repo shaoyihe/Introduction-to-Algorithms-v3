93104f2b1cc5c295a76df167ee74d34420797b9b
shaoyihe <wubuqilai@gmail.com>
2016-05-21 20:05:17 +0800

    heap

--- c12/s02/t03/Tree03.java
+++
@@ -1,5 +0,0 @@
-package c12.s02.t03;
-
-public class Tree03 {
-
-}
--- c4/p2/4.2-7.rb
+++
@@ -1,8 +0,0 @@
-def complex_multiple(a, b , c , d)
-	e = ( a - b) * c
-	f = (a + b ) * d
-	g = ( c +d )* a
-	return g - f , g - e
-end
-
-p complex_multiple(1 , 2, 3, 4)
